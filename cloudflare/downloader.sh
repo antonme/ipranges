9a8446da110630a26250aed008903e892ea4eafe
Anton Volnuhin <anton@volnuhin.ru>
2023-03-18 16:07:49 +0300

    cloudflare fix

--- cloudflare/downloader.sh
+++ cloudflare/downloader.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# https://www.workplace.com/resources/tech/it-configuration/domain-whitelisting
-# https://www.irr.net/docs/list.html
-# https://bgp.he.net/search?search%5Bsearch%5D=facebook&commit=Search
-# https://github.com/SecOps-Institute/FacebookIPLists/blob/master/facebook_asn_list.lst
 
 set -euo pipefail
 set -x
 
 curl https://www.cloudflare.com/ips-v4 > cloudflare/ipv4.txt
 curl https://www.cloudflare.com/ips-v6 > cloudflare/ipv6.txt
+
+echo >> cloudflare/ipv4.txt
+echo >> cloudflare/ipv6.txt

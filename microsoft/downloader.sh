83be6cbf47319030d72dde160125a1d846668e95
Anton Volnuhin <anton@volnuhin.ru>
2023-03-19 21:33:12 +0300

    fix ips format for microsoft (removing the column title)

--- microsoft/downloader.sh
+++ microsoft/downloader.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 set -x
 
 
-curl -s https://download.microsoft.com/download/B/2/A/B2AB28E1-DAE1-44E8-A867-4987FE089EBE/msft-public-ips.csv | cut -d',' -f1 > /tmp/microsoft.txt
+curl -s https://download.microsoft.com/download/B/2/A/B2AB28E1-DAE1-44E8-A867-4987FE089EBE/msft-public-ips.csv | cut -d',' -f1|awk 'NR > 1'  > /tmp/microsoft.txt
 
 cat /tmp/microsoft.txt | grep -v ":" > /tmp/microsoft-ipv4.txt
 cat /tmp/microsoft.txt | grep ":" > /tmp/microsoft-ipv6.txt

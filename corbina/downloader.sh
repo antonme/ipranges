068c5ed54afc82415b51782d509ff57c6ac853b2
Anton Volnuhin <anton@volnuhin.ru>
2023-03-24 00:01:10 +0300

    simple caching for ripe

--- corbina/downloader.sh
+++ corbina/downloader.sh
@@ -6,10 +6,11 @@ set -x
 
 # get from Autonomous System
 get_maintained() {
-    whois -h whois.ripe.net -- "-i mnt-by $1" | grep '^route' | awk '{ print $2; }'
+    whois -h whois.ripe.net -- "-i mnt-by $1" > /tmp/ripe.txt
+    cat /tmp/ripe.txt | grep '^route' | awk '{ print $2; }'
     whois -h whois.radb.net -- "-i mnt-by $1" | grep '^route' | awk '{ print $2; }'
     whois -h rr.ntt.net -- "-i mnt-by $1" | grep '^route' | awk '{ print $2; }'
-    whois -h whois.ripe.net -- "-i mnt-by $1" | grep inetnum|awk '{$1=""; system("ipcalc -b -r "$2" "$4"| tail +2");}'
+    cat /tmp/ripe.txt | grep inetnum|awk '{$1=""; system("ipcalc -b -r "$2" "$4"| tail +2");}'
 }
 
 

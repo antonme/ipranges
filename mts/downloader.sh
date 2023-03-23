577d4d23d7df64c386da2f83536b3f7954f1436d
Anton Volnuhin <anton@setia.io>
2023-03-23 20:44:28 +0000

    minimum ratelimiting plus cloudflare cleanup

--- mts/downloader.sh
+++ mts/downloader.sh
@@ -9,6 +9,7 @@ get_maintained() {
     whois -h whois.ripe.net -- "-i mnt-by $1" | grep '^route' | awk '{ print $2; }'
     whois -h whois.radb.net -- "-i mnt-by $1" | grep '^route' | awk '{ print $2; }'
     whois -h rr.ntt.net -- "-i mnt-by $1" | grep '^route' | awk '{ print $2; }'
+    sleep 5
     whois -h whois.ripe.net -- "-i mnt-by $1" | grep inetnum|awk '{$1=""; system("ipcalc -b -r "$2" "$4"| tail +2");}'
 }
 

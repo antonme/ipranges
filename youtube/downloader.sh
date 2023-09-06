b8ef3e2efa10aba945341d305874b895206b6c70
Anton Volnuhin <anton@volnuhin.com>
2023-09-06 18:51:24 +0300

    youtube added

---
+++ youtube/downloader.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+set -euo pipefail
+set -x
+
+
+
+# save ipv4
+grep -v ':' youtube/nets.txt > /tmp/youtube-ipv4.txt
+curl https://raw.githubusercontent.com/antonme/ipnames/master/resolve-youtube.txt >> /tmp/youtube-ipv4.txt || echo 'failed'
+curl https://raw.githubusercontent.com/antonme/ipnames/master/ext-resolve-youtube.txt >> /tmp/youtube-ipv4.txt || echo 'failed'
+
+# save ipv6
+# grep ':' youtube/nets.txt > /tmp/youtube-ipv6.txt
+
+
+# sort & uniq
+sort -h /tmp/youtube-ipv4.txt | uniq > youtube/ipv4.txt
+# sort -h /tmp/youtube-ipv6.txt | uniq > youtube/ipv6.txt
+
+
+#merge
+python utils/merge.py --source=youtube/ipv4.txt | sed 's/\/32$//'| sort -h > youtube/ipv4_merged.txt

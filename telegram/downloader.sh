7278f1fbc827147d31f0cd9a903bf9c07d09732c
Anton Volnuhin <anton@volnuhin.com>
2026-02-10 14:58:16 +0300

    telegram: more asn

--- telegram/downloader.sh
+++ telegram/downloader.sh
@@ -12,9 +12,20 @@ get_maintained() {
     whois -h whois.rogerstelecom.net -- "-i mnt-by $1" | rg '^route' | awk '{ print $2; }'
     whois -h whois.bgp.net.br -- "-i mnt-by $1" | rg '^route' | awk '{ print $2; }'
 }
+# get from Autonomous System
+get_routes() {
+    whois -h riswhois.ripe.net -- "-i origin $1" | rg '^route' | awk '{ print $2; }'
+    whois -h whois.radb.net -- "-i origin $1" | rg '^route' | awk '{ print $2; }'
+    whois -h rr.ntt.net -- "-i origin $1" | rg '^route' | awk '{ print $2; }'
+    whois -h whois.rogerstelecom.net -- "-i origin $1" | rg '^route' | awk '{ print $2; }'
+    whois -h whois.bgp.net.br -- "-i origin $1" | rg '^route' | awk '{ print $2; }'
+}
 
 get_maintained 'MNT-TELEGRAM' | grep -v 'route-set' | grep "/" > /tmp/telegram.txt || echo 'failed'
-
+get_routes 'AS62041' >> /tmp/telegram.txt || echo 'failed'
+get_routes 'AS59930' >> /tmp/telegram.txt || echo 'failed'
+get_routes 'AS44907' >> /tmp/telegram.txt || echo 'failed'
+get_routes 'AS211157' >> /tmp/telegram.txt || echo 'failed'
 
 # save ipv4
 grep -v ':' /tmp/telegram.txt > /tmp/telegram-ipv4.txt
